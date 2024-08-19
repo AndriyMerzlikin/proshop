e3c60d4c0d24b7d8c707ab0e1160b3227717074c
AndriyMerzlikin <lastheroofme@gmail.com>
2024-08-19 22:20:45 +0300

    new dirName

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,66 @@
+// import path from "path";
+// import express from "express";
+// import dotenv from "dotenv";
+// import cookieParser from "cookie-parser";
+// dotenv.config();
+// import connectDB from "./config/db.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+// const port = process.env.PORT || 5000;
+
+// connectDB();
+
+// const app = express();
+
+// // body parser middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // cookie parser middleware
+// app.use(cookieParser());
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
+
+// app.use("/api/products", productRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/orders", orderRoutes);
+// app.use("/api/upload", uploadRoutes);
+
+// app.get("/api/config/paypal", (req, res) =>
+//   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+// );
+
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
+
+// Ініціалізація __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -44,7 +103,6 @@ app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
